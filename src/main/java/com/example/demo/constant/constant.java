2ad706a7342aae83d7e7bbf235e07c18853ff37a
trandhung260704 <166836260+trandhung260704@users.noreply.github.com>
2025-06-24 10:02:55 +0700

    GoogleLogin

--- src/main/java/com/example/demo/constant/constant.java
+++ src/main/java/com/example/demo/constant/constant.java
@@ -1,8 +1,8 @@
 package com.example.demo.constant;
 
 public class constant {
-    public static final String GOOGLE_CLIENT_ID = "144168228307-d2ovtso5jq8d8u5o6okhbsoseaqqvn4c.apps.googleusercontent.com";
-    public static final String GOOGLE_CLIENT_SECRET = "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";
+    public static final String GOOGLE_CLIENT_ID = "your-google-client-id";
+    public static final String GOOGLE_CLIENT_SECRET = "your-google-client-secret";
     public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/oauth2/callback";
     public static final String GOOGLE_GRANT_TYPE = "authorization_code";
     public static final String GOOGLE_LINK_GET_TOKEN = "https://oauth2.googleapis.com/token";
